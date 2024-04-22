5c0fc21195bcd13a51815f820ca5c8a1af8031a1
Mkrishbl4z3 <151707774+Mkrishbl4z3@users.noreply.github.com>
2024-04-22 15:06:53 +0530

    Update snakegame.cpp

--- snakegame.cpp
+++ snakegame.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include,conio.h>
+#include<windows.h>
 
+ using namespace std
+ int x,y;
+ int snaketaillen;
+ int snaketailx[100];
+ int snaketaily[100];
+ int fcordx,fcordy;
+ enum snakedir{STOP = 0, LEFT ,RIGHT,UP , DOWN};
+ snakedir sdir;
+ int playerscore;
+ int width=70;
+ int height = 100;
+ 
